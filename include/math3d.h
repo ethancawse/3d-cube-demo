041315a006b430ecc05d4764af91ba2ac373bb24
3lit3hax <ethancawse@gmail.com>
2025-04-23 15:01:36 -0400

    remove old code

--- include/math3d.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <types.h>
-
-void getXRotationMatrix(float angle, float out[9]);
-void getYRotationMatrix(float angle, float out[9]);
-void getZRotationMatrix(float angle, float out[9]);
-void matrixMultiply3x3(float *, lineCoordinate *);
